7ff1d70ef349236335543df570f678299e3d6090
mata1 <matej.biberovic@gmail.com>
2013-12-10 17:04:39 +0100

    Testni overlay za dvorano A, preverjanje klika za overlay
    algoritem za preverjanje ali je klik v poligonu je z neta.. ni kake
    uporabne funkcije za to že v APIju. bedarija
    mormo se zmenit kam bojo šli gumbi za iskanje, …

--- BTC - aplikacija/src/com/fri/tpo/btc/MainActivity.java
+++ BTC - aplikacija/src/com/fri/tpo/btc/MainActivity.java
@@ -3,28 +3,35 @@ package com.fri.tpo.btc;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.Point;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnMapClickListener {
 	DatabaseConnector db;
 	Cursor cur;
 	TextView tv;
 	
 	private GoogleMap map;
+	private Polygon p; // testni poligon za dvorano A
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -97,10 +104,44 @@ public class MainActivity extends Activity {
                 return;
             }
             
-            //map.setMyLocationEnabled(true);
+            // https://developers.google.com/maps/documentation/android/views - dokumentacija za maps API
+            // listener za klik na mapo
+            map.setOnMapClickListener(this);
             // nastavi kamero na BTC
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(46.067008, 14.544182), 14));
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(46.067008, 14.544182), 15));
+            
+            // testni overlay za halo A
+            PolygonOptions overlay = new PolygonOptions();
+            overlay.add(new LatLng(46.067886, 14.541543), new LatLng(46.067961, 14.542251), new LatLng(46.065474, 14.543774), new LatLng(46.065311, 14.543066));
+            overlay.strokeColor(Color.RED);
+            overlay.strokeWidth(1f);
+            overlay.fillColor(Color.argb(50, 0, 255, 0)); // pol prosojno            
+            p = map.addPolygon(overlay);
+           
+            
         }
+    }
+    
+    @Override
+    public void onMapClick(LatLng klik) {
+    	Log.i("Klik", "klik na karto " + klik.toString());
+    	if (vsebuje(p, klik))
+    		Toast.makeText(this, "Klik na halo!", Toast.LENGTH_SHORT).show();
+    }
+    
+    // ali je izbrana tocka v poligonu
+    private boolean vsebuje(Polygon poly, LatLng point) {
+    	int i, j;
+    	List<LatLng> points = poly.getPoints();
+        boolean result = false;
+        
+        for (i = 0, j = points.size() - 1; i < points.size(); j = i++) {
+          if ((points.get(i).longitude > point.longitude) != (points.get(j).longitude > point.longitude) &&
+              (point.latitude < (points.get(j).latitude - points.get(i).latitude) * (point.longitude - points.get(i).longitude) / (points.get(j).longitude - points.get(i).longitude) + points.get(i).latitude)) {
+            result = !result;
+           }
+        }
+        return result;
     }
 
 }
